4ac5980997a50f904fd279b2e46a1c092c37c623
Newton Wamiti <144038101+Wamitinewton@users.noreply.github.com>
2025-07-09 18:20:14 +0300

    initial setup

---
+++ lib/config/router/route.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+import 'package:go_router/go_router.dart';
+
+enum AppRoute {
+  splash('/');
+  const AppRoute(this.path);
+
+  final String path;
+}
+
+extension AppRouteNavigation on AppRoute{
+  void go(BuildContext context) => context.go(path);
+
+  void push(BuildContext context) => context.push(path);
+}
